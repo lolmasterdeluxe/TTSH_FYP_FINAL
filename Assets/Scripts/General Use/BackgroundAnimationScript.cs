16e6e8636e266eb87266f12e5d329f0879bf1dd0
kwyub3 <kwyub3@users.noreply.github.com>
2021-11-26 16:30:53 +0800

    Additions
    - Added BackgroundAnimationScript
    - Made changes to Scissor Paper Stone Scene

---
+++ Assets/Scripts/General Use/BackgroundAnimationScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundAnimationScript : MonoBehaviour
+{
+    //this script handles the movement of background animation
+
+    #region Variables
+
+    Material material;
+    Vector2 imgOffset;
+
+    public float xVel, yVel;
+
+    #endregion
+
+    #region Unity Callbacks
+
+    private void Awake()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+    private void Start()
+    {
+        imgOffset = new Vector2(xVel, yVel);
+    }
+
+    private void Update()
+    {
+        material.mainTextureOffset += imgOffset * Time.deltaTime;
+    }
+    #endregion
+
+}
