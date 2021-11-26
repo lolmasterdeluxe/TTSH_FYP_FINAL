f2e6c654e8938b5b5d27353f833b29e451ff3d4a
Strigon009 <calebraymansom@gmail.com>
2021-11-26 16:19:15 +0800

    Added Chapteh scenes and scripts

---
+++ Assets/Scripts/Chapteh/MouseMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+    Vector3 mousePosition;
+    public Vector2 position = new Vector2(0f, 0f);
+    public float moveSpeed = 0.1f;
+    Rigidbody2D rb;
+
+    private float objectWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        objectWidth = GetComponent<SpriteRenderer>().bounds.size.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+
+        ClampMovement();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(position);
+    }
+
+    private void ClampMovement()
+    {
+        Vector3 position = transform.position;
+
+        float distance = transform.position.z - Camera.main.transform.position.z;
+
+        float leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance)).x + objectWidth;
+        float rightBorder = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance)).x - objectWidth;
+
+        position.x = Mathf.Clamp(position.x, leftBorder, rightBorder);
+        transform.position = position;
+    }
+}
---
+++ Assets/Scripts/Chapteh/ResetChapteh.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetChapteh : MonoBehaviour
+{
+    public Transform chapteh;
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        chapteh.position = new Vector3(chapteh.position.x, 4.43f, chapteh.position.z);
+    }
+}
