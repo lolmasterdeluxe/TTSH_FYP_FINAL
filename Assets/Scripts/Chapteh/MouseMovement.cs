8c5392b2124032ec9905739ff4222e44907eb010
Strigon009 <calebraymansom@gmail.com>
2021-11-29 16:42:19 +0800

    Added camera to follow player, dumb spawn to player position

--- Assets/Scripts/Chapteh/MouseMovement.cs
+++ Assets/Scripts/Chapteh/MouseMovement.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class MouseMovement : MonoBehaviour
 {
-    public Vector3 mousePosition;
+    private Vector3 mousePosition;
     public Vector2 playerPosition = new Vector2(0f, 0f);
     public float moveSpeed = 0.1f;
     Rigidbody2D rb;
 
     private float playerWidth;
+    public SpriteRenderer skyWidth;
 
     // Start is called before the first frame update
     void Start()
@@ -24,26 +25,13 @@ public class MouseMovement : MonoBehaviour
     {
         mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        playerPosition = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
-
-        ClampMovement();
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(playerPosition);
-    }
 
-    private void ClampMovement()
-    {
-        Vector3 position = transform.position;
-
-        float distance = transform.position.z - Camera.main.transform.position.z;
-
-        float leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance)).x + playerWidth;
-        float rightBorder = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance)).x - playerWidth;
-
-        position.x = Mathf.Clamp(position.x, leftBorder, rightBorder);
-        transform.position = position;
+        playerPosition = Vector2.Lerp(transform.position, mousePosition, moveSpeed * Time.deltaTime);
+        playerPosition.x = Mathf.Clamp(playerPosition.x, skyWidth.bounds.min.x + playerWidth, skyWidth.bounds.max.x - playerWidth);
     }
 }
