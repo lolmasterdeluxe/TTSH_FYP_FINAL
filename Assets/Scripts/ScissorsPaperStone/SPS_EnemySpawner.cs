c5c9e3c21d2dcc187de51eeeeb7c7950a99a4ab9
kwyub3 <kwyub3@users.noreply.github.com>
2021-11-30 10:13:47 +0800

    changes to name and enemyspawner script

--- Assets/Scripts/ScissorsPaperStone/SPS_EnemySpawner.cs
+++ Assets/Scripts/ScissorsPaperStone/SPS_EnemySpawner.cs
@@ -7,18 +7,7 @@ public class SPS_EnemySpawner : MonoBehaviour
 {
     //this script spawns enemies that the player encounters
 
-    [System.Serializable]
-    public class Pool
-    {
-        public string tag;
-        public GameObject prefab;
-        public int size;
-    }
-
-
     #region Variables
-
-    public List<Pool> poolList;
 
     public GameObject enemyPrefab;
 
@@ -28,28 +17,10 @@ public class SPS_EnemySpawner : MonoBehaviour
     [Tooltip("Minimum and maximum offset timing to have random pattern spawning")]
     public float min, max;
 
-    //for object pooling
-    public Dictionary<string, Queue<GameObject>> poolDictionary;
-
     #endregion
 
     private void Start()
     {
-        poolDictionary = new Dictionary<string, Queue<GameObject>>();
-
-        foreach (Pool pool in poolList)
-        {
-            Queue<GameObject> objectPool = new Queue<GameObject>();
-
-            for (int i = 0; i < pool.size; i++)
-            {
-                GameObject obj = Instantiate(pool.prefab);
-                obj.SetActive(false);
-                objectPool.Enqueue(obj);
-            }
-
-            poolDictionary.Add(pool.tag, objectPool);
-        }
     }
 
 
@@ -60,8 +31,8 @@ public class SPS_EnemySpawner : MonoBehaviour
         if (enemySpawnOffset >= Random.Range(min, max))
         {
             print("yes");
-            GameObject x = Instantiate(enemyPrefab, transform.position, transform.rotation);
-            x.transform.DOMove(new Vector3(0f, 0f, -0.5f), 2);
+            GameObject enemy = Instantiate(enemyPrefab, transform.position, transform.rotation);
+            enemy.transform.DOMove(new Vector3(0f, 0f, -0.5f), 2);
             enemySpawnOffset = 0f;
         }
 
