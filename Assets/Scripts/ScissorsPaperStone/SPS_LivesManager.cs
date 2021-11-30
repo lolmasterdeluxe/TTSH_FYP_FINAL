3ebb82cacf1287e4bc156756ca0bc96231a4a887
kwyub3 <kwyub3@users.noreply.github.com>
2021-11-30 15:39:31 +0800

    major push on changes game functions work
    - will add proper score text and UI

---
+++ Assets/Scripts/ScissorsPaperStone/SPS_LivesManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SPS_LivesManager : MonoBehaviour
+{
+    //this handles the lives of the players
+
+    #region Variables
+
+    int numberofLives;
+    public bool playerisAlive;
+
+    #endregion
+
+    #region Unity Callbacks
+
+    public void Start()
+    {
+        //we establish the number of lives a player has at the start:
+        numberofLives = 5;
+        //we establish the boolean flags on Start
+        playerisAlive = true;
+    }
+
+    #endregion
+
+    #region Functions
+
+    public void PlayerDies()
+    {
+        playerisAlive = false;
+        Debug.Log("Player has died! Game has ended xD");
+    }
+
+    public void PlayerTakesDamage()
+    {
+        numberofLives -= 1;
+        Debug.Log("You lost a life!");
+
+        if (numberofLives < 0)
+        {
+            numberofLives = 0;
+        }
+    }
+
+    #endregion
+
+}
