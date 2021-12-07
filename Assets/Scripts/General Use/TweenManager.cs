9c244e1373fcdf645ededba9e3fba262d449a8c5
mawenxi2112 <mawenxi2112@gmail.com>
2021-12-08 01:38:12 +0800

    Added tween manager

---
+++ Assets/Scripts/General Use/TweenManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TweenManager : MonoBehaviour
+{
+    private static TweenManager _instance;
+    public static TweenManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject instance = new GameObject("TweenManager");
+                instance.AddComponent<TweenManager>();
+            }
+
+            return _instance;
+        }
+    }
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+            Destroy(this.gameObject);
+        else
+            _instance = this;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    // Remove this if you want the TweenManager instance to be the same throughout the whole program
+    private void OnDestroy()
+    {
+        if (this == _instance)
+            _instance = null;
+    }
+}
