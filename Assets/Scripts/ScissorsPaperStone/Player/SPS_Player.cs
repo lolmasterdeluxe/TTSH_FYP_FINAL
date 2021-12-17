6bf0a302f03a75c02f7f51fd6b4015a8c4087fbe
TeaQube <thunderwave.chen@gmail.com>
2021-12-17 17:01:12 +0800

    changes to sprite size

--- Assets/Scripts/ScissorsPaperStone/Player/SPS_Player.cs
+++ Assets/Scripts/ScissorsPaperStone/Player/SPS_Player.cs
@@ -209,8 +209,8 @@ public class SPS_Player : MonoBehaviour
         attackCollisionInstance.AttackButtonPress();
 
         //we shift the attack animation pane such that it fits
-        playerActionAnimation.transform.localPosition = new Vector3(0.15f, -0.4f, 1f);
-        playerActionAnimation.transform.localScale = new Vector3(1f, 1f, 1f);
+        playerActionAnimation.transform.localPosition = new Vector3(0f, -0.5f, 1f);
+        playerActionAnimation.transform.localScale = new Vector3(1.5f, 1.5f, 1f);
     }
 
     public void PlayerChoosesPaper()
@@ -223,8 +223,8 @@ public class SPS_Player : MonoBehaviour
         attackCollisionInstance.AttackButtonPress();
 
         //we shift the attack animation such that it fits
-        playerActionAnimation.transform.localPosition = new Vector3(0.7f, 0.3f, 1f);
-        playerActionAnimation.transform.localScale = new Vector3(0.85f, 0.85f, 1f);
+        playerActionAnimation.transform.localPosition = new Vector3(1f, 0.3f, 1f);
+        playerActionAnimation.transform.localScale = new Vector3(1f, 1f, 1f);
     }
 
     public void PlayerChoosesStone()
