de9b5b7f2e9c26ca0d178dc9765125b7c6a78b58
Strigon009 <42129076+Strigon009@users.noreply.github.com>
2021-12-28 11:42:27 +0800

    Splash Screen in main menu scene

---
+++ Assets/Scripts/MainMenuGameScene/SplashScreen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashScreen : MonoBehaviour
+{
+    public GameObject _splashScreenGroup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(WaitFade());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Fade()
+    {
+        TweenManager.Instance.AnimateFade(_splashScreenGroup.GetComponent<CanvasGroup>(), 0f, 2f);
+    }
+
+    IEnumerator WaitFade()
+    {
+        yield return new WaitForSeconds(2f);
+        Fade();
+    }
+}
