18b60e2a7f828bdcbec4c0934ccbbd33674e8057
TeaQube <thunderwave.chen@gmail.com>
2022-01-05 11:55:43 +0800

    ui updates

--- Assets/Scripts/ScissorsPaperStone/Managers/SPS_ObjectDespawningManager.cs
+++ Assets/Scripts/ScissorsPaperStone/Managers/SPS_ObjectDespawningManager.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SPS_Despawning : MonoBehaviour
+public class SPS_ObjectDespawningManager : MonoBehaviour
 {
-    //this helps to despawn anything that did not manage to get despawned:
-    //removes it from the game scene altogether
+    //this manager handles the despawning of objects (enemies, obstacles, powerups) off-screen
 
     #region Variables
 
+    [Tooltip("Reference to Object Manager Script")]
     SPS_ObjectManager objectspawningInstance;
 
     #endregion
@@ -17,11 +17,14 @@ public class SPS_Despawning : MonoBehaviour
 
     private void Start()
     {
+        //add instance of object manager HERE
         objectspawningInstance = FindObjectOfType<SPS_ObjectManager>();
     }
 
     private void Update()
     {
+        //we set the wave completed to be false when the list is empty
+        //this starts the NEXT wave
         if (objectspawningInstance.objectWaveList.Count == 0)
         {
             objectspawningInstance.b_waveCompleted = false;
@@ -30,16 +33,17 @@ public class SPS_Despawning : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        //remove the gameobject instance in the list
+        //remove the gameobject instance from the list
         objectspawningInstance.objectWaveList.Remove(other.gameObject);
 
+        //we now destroy it
         Destroy(other.gameObject);
-        //if it has a rigidbody we destroy it
-        if (other.gameObject.GetComponent<Rigidbody>() != null)
+        //if it has a rigidbody2d, we destroy it as well
+        if (other.gameObject.GetComponent<Rigidbody2D>() != null)
         {
-            Destroy(other.gameObject.GetComponent<Rigidbody>());
+            Destroy(other.gameObject.GetComponent<Rigidbody2D>());
         }
-        Debug.Log("Obstacle OR Enemy has been destroyed");
+        Debug.Log("An object has been destroyed");
     }
 
     #endregion
