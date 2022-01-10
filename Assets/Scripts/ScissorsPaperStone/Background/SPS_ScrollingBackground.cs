7deac29e2df7b289c4b3a8c0a2ba1e8cf3a2d73a
TeaQube <thunderwave.chen@gmail.com>
2022-01-10 16:01:20 +0800

    full-on headache

---
+++ Assets/Scripts/ScissorsPaperStone/Background/SPS_ScrollingBackground.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SPS_ScrollingBackground : MonoBehaviour
+{
+    //this script handles the scrolling background
+
+    #region Variables
+
+    [Tooltip("Reference to the Parallex Background Manager")]
+    ParallexBackgroundManager parallexbackgroundmanagerInstance;
+
+
+    #endregion
+
+    #region Unity Callbacks
+
+    private void Start()
+    {
+        parallexbackgroundmanagerInstance = FindObjectOfType<ParallexBackgroundManager>();
+    }
+
+
+    private void Update()
+    {
+        if (SPS_UIManager.Instance.b_gameEnded == true)
+            return;
+
+        //ground
+        parallexbackgroundmanagerInstance.SetBackgroundOffsetVector(new Vector2(0.2f, 0f));
+        parallexbackgroundmanagerInstance.componentContainer[0].GetComponent<Renderer>().material.mainTextureOffset
+        += parallexbackgroundmanagerInstance.GetBackgroundOffsetVector() * Time.deltaTime;
+
+        //sky
+        parallexbackgroundmanagerInstance.SetBackgroundOffsetVector(new Vector2(0.05f, 0f));
+        parallexbackgroundmanagerInstance.componentContainer[1].GetComponent<Renderer>().material.mainTextureOffset
+        += parallexbackgroundmanagerInstance.GetBackgroundOffsetVector() * Time.deltaTime;
+    }
+
+    #endregion
+
+}
