fb56f1d5ca7fc7235ad7b04641a1dd9d65d852b8
TeaQube <thunderwave.chen@gmail.com>
2022-01-24 11:49:36 +0800

    commit
    added helper function to call from container to scroll between tutorial screens

--- Assets/Scripts/MainMenuGameScene/In-Game UI/TutorialScreenManager.cs
+++ Assets/Scripts/MainMenuGameScene/In-Game UI/TutorialScreenManager.cs
@@ -17,6 +17,9 @@ public class TutorialScreenManager : MonoBehaviour
     public GameObject sps_tutorialScreen, fivestones_tutorialScreen, chapteh_tutorialScreen;
     public GameObject sps_npc, fivestones_npc, chapteh_npc;
 
+    public GameObject testPanel;
+
+
     public static TutorialScreenManager instance;
     #endregion
 
@@ -26,6 +29,21 @@ public class TutorialScreenManager : MonoBehaviour
     {
         instance = this;
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            testPanel.SetActive(true);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            testPanel.SetActive(false);
+        }
+    }
+
+
 
     #endregion
 
@@ -84,20 +102,62 @@ public class TutorialScreenManager : MonoBehaviour
         }
 
     }
-
+    
+    //attached to the start and exit button
     public void ButtonHover(GameObject selectedButton)
     {
         selectedButton.transform.localScale = new Vector3(1.25f, 1.25f, 1.25f);
     }
-
+    
+    //attached to the start and exit button
     public void ButtonHoverReset(GameObject selectedButton)
     {
         selectedButton.transform.localScale = new Vector3(1f, 1f, 1f);
     }
 
+    //attached to the exit button
     public void ButtonCloseScreen(GameObject selectedScreen)
     {
         selectedScreen.SetActive(false);
+    }
+
+    //this should setactive only the current val of the screen type
+    public void ScrollToNextScreen(List<GameObject> selectedGame, int val)
+    {
+        val += 1;
+
+        //do wrapping HERE
+
+        foreach(GameObject a in selectedGame)
+        {
+            if (selectedGame.IndexOf(a) == val)
+            {
+                a.SetActive(true);
+            }
+            else
+            {
+                a.SetActive(false);
+            }
+        }     
+    }
+
+    public void ScrollToPreviousScreen(List<GameObject> selectedGame, int val)
+    {
+        val -= 1;
+
+        //do wrapping HERE
+
+        foreach (GameObject a in selectedGame)
+        {
+            if (selectedGame.IndexOf(a) == val)
+            {
+                a.SetActive(true);
+            }
+            else
+            {
+                a.SetActive(false);
+            }
+        }
     }
 
 
