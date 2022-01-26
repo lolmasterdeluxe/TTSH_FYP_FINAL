4a079c73d38ce2eb740c10d4a6c66acd6d6dae21
Strigon009 <calebraymansom@gmail.com>
2022-01-26 14:54:19 +0800

    Added indicator when chapteh out of cam

--- Assets/Scripts/Chapteh/Indicator.cs
+++ Assets/Scripts/Chapteh/Indicator.cs
@@ -5,129 +5,41 @@ using UnityEngine.UI;
 
 public class Indicator : MonoBehaviour
 {
-    #region CodeMonkey
-    //[SerializeField] private Camera uiCamera;
-    //[SerializeField] private Sprite arrowSprite;
-    //[SerializeField] private Sprite starSprite;
-
-    //private Chapteh chapteh;
-    //private Vector3 targetPosition;
-    //private RectTransform pointerRectTransform;
-    //private Image pointerImage;
-
-    //Vector3 targetPositionScreenPoint;
-    //float borderSize;
-    //bool isOffScreen;
-    #endregion
-
-    #region So Called Working
-    public GameObject Pointer;
-    public GameObject Target;
-
-    Renderer rd;
-    #endregion
+    public GameObject chapteh;
 
     // Start is called before the first frame update
     void Start()
     {
-        //chapteh = GameObject.Find("Chapteh").GetComponent<Chapteh>();
-
-        //float borderSize = 20f;
-        //targetPositionScreenPoint = Camera.main.WorldToScreenPoint(chapteh.transform.position);
-        //isOffScreen = targetPositionScreenPoint.x <= borderSize || targetPositionScreenPoint.x >= Screen.width - borderSize ||
-        //              targetPositionScreenPoint.y <= borderSize || targetPositionScreenPoint.y >= Screen.height - borderSize;
-
-        rd = GetComponent<Renderer>();
-    }
-
-    private void Awake()
-    {
-        #region CodeMonkey
-        //chapteh.transform.position = GameObject.Find("Chapteh").transform.position;
-        //pointerRectTransform = transform.Find("Pointer1").GetComponent<RectTransform>();
-        //pointerImage = transform.Find("Pointer1").GetComponent<Image>();
-
-        //Hide();
-        #endregion
     }
 
     // Update is called once per frame
     void Update()
     {
-        #region CodeMonkey
-
-        //if (isOffScreen)
-        //{
-        //    RotatePointerTowardsTargetPosition();
-
-        //    pointerImage.sprite = arrowSprite;
-        //    Vector3 cappedTargetScreenPosition = targetPositionScreenPoint;
-        //    cappedTargetScreenPosition.x = Mathf.Clamp(cappedTargetScreenPosition.x, borderSize, Screen.width - borderSize);
-        //    cappedTargetScreenPosition.y = Mathf.Clamp(cappedTargetScreenPosition.y, borderSize, Screen.height - borderSize);
-
-        //    Vector3 pointerWorldPosition = uiCamera.ScreenToWorldPoint(cappedTargetScreenPosition);
-        //    pointerRectTransform.position = pointerWorldPosition;
-        //    pointerRectTransform.localPosition = new Vector3(pointerRectTransform.localPosition.x, pointerRectTransform.localPosition.y, 0f);
-        //}
-        //else
-        //{
-        //    pointerImage.sprite = starSprite;
-        //    Vector3 pointerWorldPosition = uiCamera.ScreenToWorldPoint(targetPositionScreenPoint);
-        //    pointerRectTransform.position = pointerWorldPosition;
-        //    pointerRectTransform.localPosition = new Vector3(pointerRectTransform.localPosition.x, pointerRectTransform.localPosition.y, 0f);
-
-        //    pointerRectTransform.localEulerAngles = Vector3.zero;
-        //}
-        #endregion
-
-        #region So Called Working
-        if (rd.isVisible == false)
-        {
-            if (Pointer.activeSelf == false)
-            {
-                Pointer.SetActive(true);
-            }
-
-            Vector2 direction = Target.transform.position - transform.position;
-
-            RaycastHit2D ray = Physics2D.Raycast(transform.position, direction);
-
-            if (ray.collider != null)
-            {
-                Pointer.transform.position = ray.point;
-            }
-        }
+        if (!ChaptehGameManager.Instance.m_gameStarted)
+            return;
         else
         {
-            if (Pointer.activeSelf == true)
+            if(!chapteh.GetComponent<SpriteRenderer>().isVisible)
             {
-                Pointer.SetActive(false);
+                transform.GetChild(0).gameObject.SetActive(true);
+
+                Vector3 screenCenter = new Vector3(Screen.width, Screen.height, 0) / 2;
+
+                Vector3 targetPositionScreenPoint = Camera.main.WorldToScreenPoint(chapteh.transform.position);
+
+                Vector3 dir = (targetPositionScreenPoint - screenCenter).normalized;
+
+                float angle = Mathf.Atan2(dir.y, dir.x);
+
+                transform.position = screenCenter + new Vector3(Mathf.Cos(angle) * screenCenter.x * 0.9f,
+                                                                Mathf.Sin(angle) * screenCenter.y * 0.9f, 0);
+
+                transform.rotation = Quaternion.Euler(0, 0, angle * Mathf.Rad2Deg);
+            }
+            else
+            {
+                transform.GetChild(0).gameObject.SetActive(false);
             }
         }
-        #endregion
     }
-
-    #region CodeMonkey
-    //private void RotatePointerTowardsTargetPosition()
-    //{
-    //    Vector3 toPosition = chapteh.transform.position;
-    //    Vector3 fromPosition = Camera.main.transform.position;
-    //    fromPosition.z = 0f;
-
-    //    Vector2 dir = (toPosition - fromPosition).normalized;
-    //    float angle = (Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg) % 360;
-    //    pointerRectTransform.localEulerAngles = new Vector3(0, 0, angle);
-    //}
-
-    //public void Hide()
-    //{
-    //    gameObject.SetActive(false);
-    //}
-
-    //public void Show(Vector3 targetPosition)
-    //{
-    //    gameObject.SetActive(true);
-    //    chapteh.transform.position = targetPosition;
-    //}
-    #endregion
 }
