a5a67b8e81d7d7a3f6323a568660da5fc5e5c7ce
ZavierChew <69611462+ZavierChew@users.noreply.github.com>
2022-03-31 14:59:22 +0800

    made animations for turning the eraser
    also made it a 3D object

--- Assets/Scripts/CountryErasers/MainEraser.cs
+++ Assets/Scripts/CountryErasers/MainEraser.cs
@@ -1,10 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class MainEraser : MonoBehaviour
 {
     [SerializeField] private GameObject cardBack;
+    [SerializeField] private GameObject cardFront;
     [SerializeField] private SceneController controller;
     private int _id;
     public int id
@@ -12,28 +12,43 @@ public class MainEraser : MonoBehaviour
         get { return _id; }
     }
 
-    private void Awake()
-    {
-        cardBack = transform.GetChild(0).gameObject;
-    }
+   //private void Awake()
+   //{
+   //    cardBack = transform.GetChild(0).gameObject;
+   //}
     private void OnMouseDown()
     {
         if (cardBack.activeSelf && controller.canReveal)
         {
-            cardBack.SetActive(!cardBack.activeSelf);
+            //cardBack.SetActive(!cardBack.activeSelf);
+            GetComponent<Animator>().SetTrigger("onClick");
             controller.EraserRevealed(this);
             print("revealing");
         }
     }
 
-    public void ChangeSprite(int id, Sprite Image)
+    public void ChangeSprite(int id, Material material)
     {
         _id = id;
-        GetComponent<SpriteRenderer>().sprite = Image;
+        transform.GetChild(1).GetComponent<MeshRenderer>().material = material;
     }
 
     public void Cover()
     {
-        cardBack.SetActive(true);
+        //cardBack.SetActive(true);
+        GetComponent<Animator>().SetTrigger("onClick");
+        Invoke("OnIdle",1.3f);
+    }
+
+    public void ChangeLayer()
+    {
+        int temp = cardBack.GetComponent<SpriteRenderer>().sortingOrder;
+        cardBack.GetComponent<SpriteRenderer>().sortingOrder = cardFront.GetComponent<SpriteRenderer>().sortingOrder;
+        cardFront.GetComponent<SpriteRenderer>().sortingOrder = temp;
+    }
+
+    private void OnIdle()
+    {
+        GetComponent<Animator>().SetTrigger("onIdle");
     }
 }
