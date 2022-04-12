d8f27dcb9ae287a4b9ffe247b8f65cea468c8720
lolmaster deluxe <kiddrifdi@gmail.com>
2022-04-12 14:20:04 +0800

    Fixed tutorials buttons interactable when not appearing or in customize screen
    Made leaderboard to currently work only in Unity while it still does not work on mobile

--- Assets/Scripts/MainMenuGameScene/In-Game UI/InteractNPC.cs
+++ Assets/Scripts/MainMenuGameScene/In-Game UI/InteractNPC.cs
@@ -7,34 +7,22 @@ using DG.Tweening;
 
 public class InteractNPC : MonoBehaviour
 {
-    // Start is called before the first frame update
-
-    public enum NPC_TYPE
-    {
-        FIVE_STONES,
-        SPS,
-        CHAPTEH,
-        FLAPPY,
-        COUNTRY_ERASERS,
-        CUSTOMIZER,
-        LEADERBOARD
-    }
-
-    public NPC_TYPE type;
     TutorialScreenManager tutorialscreenmanagerInstance;
 
     //for interact HUD
-    public GameObject interactPrefab;
+    public GameObject interactButton;
 
     //audioSource variables
-    public AudioSource interactSFX, hoverSFX;
+    public AudioSource hoverSFX;
     public AudioSource BGM;
+
+    private bool InRange = false;
 
     void Start()
     {
         tutorialscreenmanagerInstance = TutorialScreenManager.instance;
-        interactPrefab.transform.GetComponent<Image>().DOFade(0f, 0f);
-        interactPrefab.transform.GetChild(0).GetComponent<Image>().DOFade(0f, 0f);
+        interactButton.transform.GetComponent<Image>().DOFade(0f, 0f);
+        interactButton.transform.GetChild(0).GetComponent<Image>().DOFade(0f, 0f);
     }
 
     // Update is called once per frame
@@ -53,166 +41,26 @@ public class InteractNPC : MonoBehaviour
             //reset audio volume
             BGM.volume = 0.2f;
         }
+
+        if (InRange)
+            interactButton.SetActive(true);
+        else if (interactButton.transform.GetComponent<Image>().color.a <= 0)
+            interactButton.SetActive(false);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        switch (type)
-        {
-            case NPC_TYPE.SPS:
-                interactPrefab.transform.GetComponent<Image>().DOFade(1f, 1.25f);
-                interactPrefab.transform.GetChild(0).GetComponent<Image>().DOFade(1f, 1.25f);
-                hoverSFX.Play();
-                if (Input.GetKeyDown(KeyCode.F))
-                    tutorialscreenmanagerInstance.TutorialScreenOpen("SPS");
-                break;
-            case NPC_TYPE.FIVE_STONES:
-                interactPrefab.transform.GetComponent<Image>().DOFade(1f, 1.25f);
-                interactPrefab.transform.GetChild(0).GetComponent<Image>().DOFade(1f, 1.25f);
-                hoverSFX.Play();
-                if (Input.GetKeyDown(KeyCode.F))
-                    tutorialscreenmanagerInstance.TutorialScreenOpen("FIVESTONES");
-                break;
-            case NPC_TYPE.CHAPTEH:
-                interactPrefab.transform.GetComponent<Image>().DOFade(1f, 1.25f);
-                interactPrefab.transform.GetChild(0).GetComponent<Image>().DOFade(1f, 1.25f);
-                hoverSFX.Play();
-                if (Input.GetKeyDown(KeyCode.F))
-                    tutorialscreenmanagerInstance.TutorialScreenOpen("CHAPTEH");
-                break;
-            case NPC_TYPE.FLAPPY:
-                interactPrefab.transform.GetComponent<Image>().DOFade(1f, 1.25f);
-                interactPrefab.transform.GetChild(0).GetComponent<Image>().DOFade(1f, 1.25f);
-                hoverSFX.Play();
-                if (Input.GetKeyDown(KeyCode.F))
-                    tutorialscreenmanagerInstance.TutorialScreenOpen("CHAPTEH");
-                break;
-            case NPC_TYPE.COUNTRY_ERASERS:
-                interactPrefab.transform.GetComponent<Image>().DOFade(1f, 1.25f);
-                interactPrefab.transform.GetChild(0).GetComponent<Image>().DOFade(1f, 1.25f);
-                hoverSFX.Play();
-                if (Input.GetKeyDown(KeyCode.F))
-                    tutorialscreenmanagerInstance.TutorialScreenOpen("CHAPTEH");
-                break;
-            case NPC_TYPE.CUSTOMIZER:
-                interactPrefab.transform.GetComponent<Image>().DOFade(1f, 1.25f);
-                interactPrefab.transform.GetChild(0).GetComponent<Image>().DOFade(1f, 1.25f);
-                hoverSFX.Play();
-                /*if (Input.GetKeyDown(KeyCode.F))
-                    SceneManager.LoadScene("CustomizeScene");*/
-                break;
-            case NPC_TYPE.LEADERBOARD:
-                // Recode this later on
-                interactPrefab.transform.GetComponent<Image>().DOFade(1f, 1.25f);
-                interactPrefab.transform.GetChild(0).GetComponent<Image>().DOFade(1f, 1.25f);
-                hoverSFX.Play();
-                if (Input.GetKeyDown(KeyCode.F))
-                    Resources.FindObjectsOfTypeAll<LeaderboardManager>()[0].gameObject.SetActive(true);
-                break;
-        }
-    }
-
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        switch (type)
-        {
-            case NPC_TYPE.SPS:
-                if (Input.GetKeyDown(KeyCode.F))
-                {
-                    interactSFX.Play();
-                    BGM.volume = 0.05f;
-                    tutorialscreenmanagerInstance.TutorialScreenOpen("SPS");
-                }
-                break;
-            case NPC_TYPE.FIVE_STONES:
-                if (Input.GetKeyDown(KeyCode.F))
-                {
-                    interactSFX.Play();
-                    BGM.volume = 0.05f;
-                    tutorialscreenmanagerInstance.TutorialScreenOpen("FIVESTONES");
-                }
-                break;
-            case NPC_TYPE.CHAPTEH:
-                if (Input.GetKeyDown(KeyCode.F))
-                {
-                    interactSFX.Play();
-                    BGM.volume = 0.05f;
-                    tutorialscreenmanagerInstance.TutorialScreenOpen("CHAPTEH");
-                }   
-                break;
-            case NPC_TYPE.FLAPPY:
-                if (Input.GetKeyDown(KeyCode.F))
-                {
-                    interactSFX.Play();
-                    BGM.volume = 0.05f;
-                    //tutorialscreenmanagerInstance.TutorialScreenOpen(TutorialScreenManager.TutorialScreenType.FLAPPY);
-                    tutorialscreenmanagerInstance.StartGameButtonPressed(3);
-                }
-                break;
-            case NPC_TYPE.COUNTRY_ERASERS:
-                if (Input.GetKeyDown(KeyCode.F))
-                {
-                    interactSFX.Play();
-                    BGM.volume = 0.05f;
-                    //tutorialscreenmanagerInstance.TutorialScreenOpen(TutorialScreenManager.TutorialScreenType.COUNTRY_ERASERS);
-                    tutorialscreenmanagerInstance.StartGameButtonPressed(4);
-                }
-                break;
-            case NPC_TYPE.CUSTOMIZER:
-                if (Input.GetKeyDown(KeyCode.F))
-                {
-                    interactSFX.Play();
-                    BGM.volume = 0.05f;
-                    SceneManager.LoadScene("CustomizeScene");
-                }
-                break;
-            case NPC_TYPE.LEADERBOARD:
-                // Recode this later on
-                if (Input.GetKeyDown(KeyCode.F))
-                {
-                    interactSFX.Play();
-                    BGM.volume = 0.05f;
-                    Resources.FindObjectsOfTypeAll<LeaderboardManager>()[0].gameObject.SetActive(true);
-                }
-                break;
-        }
+        InRange = true;
+        interactButton.transform.GetComponent<Image>().DOFade(1f, 1.25f);
+        interactButton.transform.GetChild(0).GetComponent<Image>().DOFade(1f, 1.25f);
+        hoverSFX.Play();
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        switch (type)
-        {
-            case NPC_TYPE.SPS:
-                interactPrefab.transform.GetComponent<Image>().DOFade(0f, .75f);
-                interactPrefab.transform.GetChild(0).GetComponent<Image>().DOFade(0f, .75f);
-                break;
-            case NPC_TYPE.FIVE_STONES:
-                interactPrefab.transform.GetComponent<Image>().DOFade(0f, .75f);
-                interactPrefab.transform.GetChild(0).GetComponent<Image>().DOFade(0f, .75f);
-                break;
-            case NPC_TYPE.CHAPTEH:
-                interactPrefab.transform.GetComponent<Image>().DOFade(0f, .75f);
-                interactPrefab.transform.GetChild(0).GetComponent<Image>().DOFade(0f, .75f);
-                break;
-            case NPC_TYPE.FLAPPY:
-                interactPrefab.transform.GetComponent<Image>().DOFade(0f, .75f);
-                interactPrefab.transform.GetChild(0).GetComponent<Image>().DOFade(0f, .75f);
-                break;
-            case NPC_TYPE.COUNTRY_ERASERS:
-                interactPrefab.transform.GetComponent<Image>().DOFade(0f, .75f);
-                interactPrefab.transform.GetChild(0).GetComponent<Image>().DOFade(0f, .75f);
-                break;
-            case NPC_TYPE.CUSTOMIZER:
-                interactPrefab.transform.GetComponent<Image>().DOFade(0f, .75f);
-                interactPrefab.transform.GetChild(0).GetComponent<Image>().DOFade(0f, .75f);
-                break;
-            case NPC_TYPE.LEADERBOARD:
-                interactPrefab.transform.GetComponent<Image>().DOFade(0f, .75f);
-                interactPrefab.transform.GetChild(0).GetComponent<Image>().DOFade(0f, .75f);
-                break;
-        }
+        InRange = false;
+        interactButton.transform.GetComponent<Image>().DOFade(0f, .75f);
+        interactButton.transform.GetChild(0).GetComponent<Image>().DOFade(0f, .75f);
     }
-
 
 }
